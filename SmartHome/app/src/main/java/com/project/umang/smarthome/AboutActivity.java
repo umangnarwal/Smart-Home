bcc2c003869dfc262f3ad40131edc2310b5a8ae6
umangnarwal <iit2015070@iiita.ac.in>
2018-05-17 22:26:33 +0530

    Smart Home
    IoT Android Application

---
+++ SmartHome/app/src/main/java/com/project/umang/smarthome/AboutActivity.java
@@ -0,0 +1,45 @@
+package com.project.umang.smarthome;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+
+public class AboutActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+
+    public void fb(View view)
+    {
+        Intent fbIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/hunny.narwal"));
+        startActivity(fbIntent);
+    }
+
+    public void tweet(View view)
+    {
+        Intent tweetIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://twitter.com/mayoogh1997/"));
+        startActivity(tweetIntent);
+    }
+
+    public void insta(View view)
+    {
+        Intent instaIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.instagram.com/sicl3lement/"));
+        startActivity(instaIntent);
+    }
+
+
+    public void web(View view)
+    {
+        Intent webIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://igniteinnovateideas.wordpress.com/"));
+        startActivity(webIntent);
+    }
+}
---
+++ SmartHome/app/src/main/java/com/project/umang/smarthome/SplashScreen.java
@@ -0,0 +1,41 @@
+package com.project.umang.smarthome;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+/**
+ * Created by PEACE on 3/30/2016.
+ */
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+        Log.d("myTag", "This is my message");
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(700);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashScreen.this,DeviceList.class);
+                    startActivity(intent);
+                    Log.d("myTag", "This is my message");
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+
+}
